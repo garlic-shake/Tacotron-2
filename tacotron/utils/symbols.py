664fe87a6f7df8e6277af22726f2218c71ddc9c4
Astw <tekinoku@gmail.com>
2019-11-22 16:00:49 +0600

    new symbols with stress

--- tacotron/utils/symbols.py
+++ tacotron/utils/symbols.py
@@ -8,11 +8,17 @@ from . import cmudict
 
 _pad        = '_'
 _eos        = '~'
+_punctuation = '!\'(),.:;?- '
 #_characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!\'\"(),-.:;? '
-_characters = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя!\'\"(),-.:;? '
-
+_characters_ru = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+_svowels_ru = ['а́', 'и́', 'у́', 'э́', 'о́', 'ю́', 'я́', 'ы́', 'е́']
+_stress_vowels = {'á': 'а', 'ó': 'о', 'é': 'е', 'ý': 'у',
+                  'а́': 'а', 'и́': 'и', 'у́': 'у', 'э́': 'э', 'о́': 'о', 'ю́': 'ю', 'я́': 'я', 'ы́': 'ы', 'е́': 'е'}
+_stress_vowels_inv = {v: k for k, v in _stress_vowels.items()}
 # Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
 #_arpabet = ['@' + s for s in cmudict.valid_symbols]
 
 # Export all symbols:
-symbols = [_pad, _eos] + list(_characters) #+ _arpabet
+# symbols = [_pad, _eos] + list(_characters) #+ _arpabet
+ru_symbols = [_pad, _eos] + list(_punctuation) + list(_characters_ru) + _svowels_ru
+symbols = ru_symbols
